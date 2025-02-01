fb9a0e59c1302b2cc09ad40312912da875e401c0
Guillermo <guillermoeguia92@gmail.com>
2025-02-01 19:31:44 -0300

    Agrega logica de ingreso y sorteo de amigos

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  let nombre = document.getElementById("amigo").value;
+
+  if (nombre !== "") {
+    amigos.push(nombre);
+    console.log(amigos);
+    limpiarInput();
+    mostrarAmigos();
+  } else {
+    alert("Porfavor, inserte un nombre");
+  }
+}
+
+function limpiarInput() {
+  document.getElementById("amigo").value = "";
+}
+
+function mostrarAmigos() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    let item = document.createElement("li");
+    item.textContent = amigos[i];
+    lista.appendChild(item);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length < 2) {
+    alert("Necesitas al menos 2 amigos para sortear");
+    return;
+  } else {
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    document.getElementById("resultado").textContent = amigoSorteado;
+  }
+}
